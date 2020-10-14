24748101d8623a95eb2ff7d98b077a92a3afbd01
awesomeness.a <ang7regio@gmail.com>
2020-10-14 08:15:40 +0300

    add a file with ChatBot

---
+++ generative_chatbot/chat.py
@@ -0,0 +1,89 @@
+import numpy as np
+import re
+from test_model import encoder_model, decoder_model, num_decoder_tokens, num_encoder_tokens, input_features_dict, target_features_dict, reverse_target_features_dict, max_decoder_seq_length, max_encoder_seq_length
+
+class ChatBot:
+    # a list of words and phrases that indicate 
+    # the user's response is negative
+    negative_responses = ["no", "nope", "exit", "goodbye", "bye", "later", "next time", "stop"]
+
+    # a list of words and phrases that should 
+    # terminate the chatbot conversation
+    exit_commands = ["quit", "pause", "stop", "exit", "goodbye", "bye", "later"]
+
+    # start_chat() greets the user, and checks
+    # if the user wants to chat
+    def start_chat(self):
+        user_response = input("Hi there! Would you like to chat with me?\n")
+        
+        if user_response in self.negative_responses:
+            print("Ok, have a great day!")
+            return
+
+        self.chat(user_response)
+
+
+    def chat(self, reply):
+        while not self.make_exit(reply):
+            reply = input(self.generate_response(reply))
+
+
+    # string_to_matrix() accepts user input and 
+    # creates a NumPy matrix of one-hot vectors
+    def string_to_matrix(self, user_input):
+        tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
+        user_input_matrix = np.zeros(
+            (1, max_encoder_seq_length, num_encoder_tokens),
+            dtype='float32')
+        for timestep, token in enumerate(tokens):
+            if token in input_features_dict:
+                user_input_matrix[0, timestep, input_features_dict[token]] = 1.
+            return user_input_matrix
+
+
+    # generate_response() copies the body of decode_sequence()
+    # with a few modifications
+    def generate_response(self, user_input):
+        input_matrix = self.string_to_matrix(user_input)
+        states_value = encoder_model.predict(input_matrix)
+        
+        target_seq = np.zeros(
+            (1, 1, num_decoder_tokens))
+        target_seq[0, 0, target_features_dict['<START>']] = 1.
+        
+        chatbot_response = ''
+        stop_condition = False
+        
+        while not stop_condition:
+            output_tokens, hidden_state, cell_state = decoder_model.predict(
+                [target_seq] + states_value)
+            sampled_token_index = np.argmax(output_tokens[0, -1, :])
+            sampled_token = reverse_target_features_dict[sampled_token_index]
+            chatbot_response += " " + sampled_token
+            
+            if (sampled_token == '<END>' or len(chatbot_response) > max_decoder_seq_length):
+                stop_condition = True
+            target_seq = np.zeros(
+                (1, 1, num_decoder_tokens))
+            target_seq[0, 0, sampled_token_index] = 1.
+            states_value = [hidden_state, cell_state]
+        
+        chatbot_response = chatbot_response.replace("<START>", "").replace("<END>", "")
+        
+        return chatbot_response
+
+    # make_exit() checks if any exit commands are in the user's reply
+    def make_exit(self, reply):
+        for exit_command in self.exit_commands:
+            if exit_command in reply:
+                print("Ok, have a great day!")
+                return True
+
+            return False
+
+
+# instantiate a new chatbot
+my_chatbot = ChatBot()
+
+# let it chat!
+my_chatbot.start_chat()
